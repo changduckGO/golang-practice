969b7e6b2449acad69f7117687ea27428eb566be
SCD <scd0317@gmail.com>
2022-11-16 11:14:02 +0900

    #1.4 Functions part Two

--- main.go
+++ main.go
@@ -7,17 +7,14 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (lenght int, upperName string) {
+	defer fmt.Println("I'm done!")
+	lenght = len(name)
+	upperName = strings.ToUpper(name)
+	return
 }
 
 func main() {
 	totalLength, upperName := lenAndUpper("changduck")
 	fmt.Println(totalLength, upperName)
-
-	repeatMe("cd", "harry", "SCD", "duck")
 }
