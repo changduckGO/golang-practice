6fbe4031b300a959d3bf633fd96abe821dfb593d
SCD <scd0317@gmail.com>
2022-12-05 00:18:13 +0900

    #3.6 URL Checker + Goroutines

--- main.go
+++ main.go
@@ -3,26 +3,46 @@ package main
 import (
 	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
+
+type result struct {
+	url    string
+	status string
+}
 
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
-	channel := make(chan string)
-	people := [5]string{"harry", "SCD", "CHAN", "DUCK", "HARRY POTTER"}
+	channel := make(chan result)
 
-	for _, person := range people {
-		go sexyCount(person, channel)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
 
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-channel) // <-channel은 일종의 blocking operation
-		// 다시 말해서 main 함수에서 무언가를 받기 전까지는 동작을 멈춘다는 의미
+	for _, url := range urls {
+		go hitURL(url, channel)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-channel)
 	}
 }
 
-func sexyCount(person string, channel chan string) {
-	time.Sleep(time.Second * 5)
-	channel <- person + " is sexy"
+func hitURL(url string, channel chan<- result) {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	channel <- result{url: url, status: status}
 }
